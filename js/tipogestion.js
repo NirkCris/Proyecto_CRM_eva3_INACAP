fb970957ad09cf6a8f71b3fe5933925c20ef016f
NirkCris <cristobal.quidel97@gmail.com>
2024-06-17 22:05:48 -0400

    Se agregaron html actualizar y eliminar

--- js/tipogestion.js
+++ js/tipogestion.js
@@ -50,8 +50,8 @@ function completarFila(element,index,arr) {
   <td>${element.nombre_tipo_gestion}</td>
   <td>${element.fecha_registro}</td>
   <td>
-  <a href='actualizar.html?id='${element.id_tipo_gestion}' class='btn btn-warning btn-sm'>Actualizar</a>
-  <a href='eliminar.html?id='${element.id_tipo_gestion}' class='btn btn-danger btn-sm'>Eliminar</a>
+  <a href='actualizar.html?id=${element.id_tipo_gestion}' class='btn btn-warning btn-sm'>Actualizar</a>
+  <a href='eliminar.html?id=${element.id_tipo_gestion}' class='btn btn-danger btn-sm'>Eliminar</a>
   </td>
   </tr>`
 }
@@ -64,25 +64,26 @@ function obtenerIdActualizar() {
   obtenerDatosActualizar(p_id_tipo_gestion);
 }
 
-function obtenerIdEliminar() {
-  const queryString = window.location.search;
-  const parametros = new URLSearchParams(queryString);
-  const p_id_tipo_gestion = parametros.get('id');
-  g_id_tipo_gestion = p_id_tipo_gestion;
-  obtenerDatosActualizar(p_id_tipo_gestion);
-}
 
 function obtenerDatosActualizar (p_id_tipo_gestion) {
   const requestOptions = {
     method: "GET",
     redirect: "follow"
   };
-
+  
   fetch("http://144.126.210.74:8080/api/tipo_gestion/"+p_id_tipo_gestion, requestOptions)
-    .then((response) => response.json())
-    .then((json) => json.forEach(completarFormulario))
-    .then((result) => console.log(result))
-    .catch((error) => console.error(error));
+  .then((response) => response.json())
+  .then((json) => json.forEach(completarFormulario))
+  .then((result) => console.log(result))
+  .catch((error) => console.error(error));
+}
+
+function obtenerIdEliminar() {
+  const queryString = window.location.search;
+  const parametros = new URLSearchParams(queryString);
+  const p_id_tipo_gestion = parametros.get('id');
+  g_id_tipo_gestion = p_id_tipo_gestion;
+  obtenerDatosEliminar(p_id_tipo_gestion);
 }
 
 function obtenerDatosEliminar (p_id_tipo_gestion) {
@@ -90,12 +91,41 @@ function obtenerDatosEliminar (p_id_tipo_gestion) {
     method: "GET",
     redirect: "follow"
   };
-
+  
   fetch("http://144.126.210.74:8080/api/tipo_gestion/"+p_id_tipo_gestion, requestOptions)
-    .then((response) => response.json())
-    .then((json) => json.forEach(completarEtiqueta))
-    .then((result) => console.log(result))
-    .catch((error) => console.error(error));
+  .then((response) => response.json())
+  .then((json) => json.forEach(completarEtiquetaEliminar))
+  .then((result) => console.log(result))
+  .catch((error) => console.error(error));
+}
+
+function completarEtiquetaEliminar(element,index,arr) {
+  var nombre_tipo_gestion = element.nombre_tipo_gestion;
+  document.getElementById('lbl_eliminar').innerHTML = "¿Desea eliminar el tipo de gestión <strong>"+ nombre_tipo_gestion +"</strong> para siempre?";
+}
+
+function eliminarTipoGestion() {
+  
+  const myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+  
+  const requestOptions = {
+    method: "DELETE",
+    headers: myHeaders,
+    redirect: "follow"
+  };
+  
+  fetch("http://144.126.210.74:8080/api/tipo_gestion/"+ g_id_tipo_gestion, requestOptions)
+  .then((response) => {
+    if(response.status == 200){
+      location.href="listar.html";
+    }
+    if(response.status == 400){
+      alert("No se puede eliminar el tipo de gestión")
+    }
+  })
+  .then((result) => console.log(result))
+  .catch((error) => console.error(error));
 }
 
 function completarFormulario(element,index,arr) {
@@ -103,56 +133,32 @@ function completarFormulario(element,index,arr) {
   document.getElementById('txt_nombre_tipo_gestion').value = nombre_tipo_gestion;
 }
 
-function completarEtiqueta(element,index,arr) {
-  var nombre_tipo_gestion = element.nombre_tipo_gestion;
-  document.getElementById('lbl_eliminar').innerHTML = "¿Desea eliminar este tipo de gestión?"+ nombre_tipo_gestion;
-}
-
 function actualizarTipoGestion() {
   //obtenemos el valor de tipo de gestion desde el formulario
   var nombre_tipo_gestion = document.getElementById("txt_nombre_tipo_gestion").value;
-
+  
   const myHeaders = new Headers();
   myHeaders.append("Content-Type", "application/json");
-
+  
   const raw = JSON.stringify({
     "nombre_tipo_gestion": nombre_tipo_gestion,
   });
-
+  
   const requestOptions = {
     method: "PATCH",
     headers: myHeaders,
     body: raw,
     redirect: "follow"
   };
-
+  
   fetch("http://144.126.210.74:8080/api/tipo_gestion/"+ g_id_tipo_gestion, requestOptions)
-    .then((response) => {
-      if(response.status == 200){
-        location.href="listar.html";
-      }
-    })
-    .then((result) => console.log(result))
-    .catch((error) => console.error(error));
+  .then((response) => {
+    if(response.status == 200){
+      location.href="listar.html";
+    }
+  })
+  .then((result) => console.log(result))
+  .catch((error) => console.error(error));
 }
 
-function eliminarTipoGestion() {
 
-  const myHeaders = new Headers();
-  myHeaders.append("Content-Type", "application/json");
-
-  const requestOptions = {
-    method: "DELETE",
-    headers: myHeaders,
-    redirect: "follow"
-  };
-
-  fetch("http://144.126.210.74:8080/api/tipo_gestion/"+ g_id_tipo_gestion, requestOptions)
-    .then((response) => {
-      if(response.status == 200){
-        location.href="listar.html";
-      }
-    })
-    .then((result) => console.log(result))
-    .catch((error) => console.error(error));
-}
